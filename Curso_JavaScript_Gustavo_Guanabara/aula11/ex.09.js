5a1b662434d2b7ba7e6fe0425d1a37f3e0ce308d
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2022-08-16 07:09:16 -0300

    16/08

---
+++ Curso_JavaScript_Gustavo_Guanabara/aula11/ex.09.js
@@ -0,0 +1,15 @@
+//utilizando a condição composta. Se a pessoa Nasceu no Brasil é brasileira se não for é estrangeira
+
+pais = "brasil"
+if  (pais == "brasil") {
+    console.log("Voce é brasileiro(a) nasceu no Brasil")
+}else{
+    console.log(`Voce é estrangeiro nasceu no ${pais}`)
+}
+
+pais = "brasil"
+if  (pais != "brasil") {
+    console.log("Voce é entrangeiro")
+}else{
+    console.log(`Voce é brasiileiro`)
+}
---
+++ Curso_JavaScript_Gustavo_Guanabara/aula11/ex08.js
@@ -0,0 +1,17 @@
+// baixei a extensão node.js exec
+//console.log("O console está funcionando corretamente!")
+/*________________________________________________________*/
+/*
+var verlocdiade = 60.5
+console.log(`a velocidade do seu carro é ${velocidade}kmh`)
+console.log("Você ultrapassou a velocidade permitida. MULTADO")
+console.log("Dirija sempre usando cinto de segurança")
+*/
+/*________________________________________________________*/
+// Se a velocidade for maior que 60 é multado. Usando condição simples
+var velocidade = 12;
+console.log(`A velocidade so seu carro é ${velocidade}`) //console.log serve para imprimir na tela 
+if (velocidade > 60) { //condição simples é quando não não tem o se não não usa
+    console.log("Você ultrapassou a velocidade permitida. MULTADO")
+}  
+console.log("Dirija sempre usando cinto de segurança")
