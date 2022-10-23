3181ce9d4bf4bafb438aaa82cd71310920a73325
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2022-10-23 19:49:13 -0300

    novos programas

--- 1-Projetos/1-Projetos-Sites/6. Projeto - Portfólio da Vinci/script.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('cor1').addEventListener("click", function(){
-        document.querySelector("body").setAttribute("id","cor1")
-    })
-
-
---
+++ 1-Projetos/1-Projetos-Sites/6. Projeto - Site Leonardo da Vinci (100%)/script.js
@@ -0,0 +1,12 @@
+
+/*document.getElementById('cor1').addEventListener("click", function(){
+        document.querySelector("body").setAttribute("id","cor1")
+    })*/
+
+
+
+
+let a1 = 5
+let a2 = 10
+let soma = a1 + a2
+console.log(soma)
